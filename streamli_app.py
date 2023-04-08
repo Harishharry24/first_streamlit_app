513f23d88e7d0ac1cb8793d0c3d359bdfa3aa055
Harishharry24 <130252325+Harishharry24@users.noreply.github.com>
2023-04-08 22:16:48 +0100

    Update streamli_app.py

--- streamli_app.py
+++ streamli_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('Harish had dinner')
 
 streamlit.title( 'chapathi with ghee and peas')
+
+streamlit.text('hey how are you')
+
+streamlit.header('harish will get job in Amazon')
