2fe212f9d36de5b861db3f192c239b0718125ca2
Harishharry24 <130252325+Harishharry24@users.noreply.github.com>
2023-04-08 22:21:14 +0100

    Update streamli_app.py
    I  have added icons before the inverted commas so it didnt worked

--- streamli_app.py
+++ streamli_app.py
@@ -8,7 +8,7 @@ streamlit.text('hey how are you')
 
 streamlit.header('harish will get job in Amazon')
 
-streamlit.header(🥣 🥗 🐔 🥑🍞'Breakfast Menu'🥣 🥗 🐔 🥑🍞)
-streamlit.text(🥣 🥗 🐔 🥑🍞🥣 🥗 🐔 🥑🍞🥣 🥗 🐔 🥑🍞'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥣 🥗 🐔 🥑🍞🥣 🥗 🐔 🥑🍞'Kale, Spinach & Rocket Smoothie'🥣 🥗 🐔 🥑🍞🥣 🥗 🐔 🥑🍞🥣 🥗 🐔 🥑🍞)
-streamlit.text(🥣 🥗 🐔 🥑🍞'Hard-Boiled Free-Range Egg'🥣 🥗 🐔 🥑🍞)
+streamlit.header('🥣 🥗 🐔 🥑🍞Breakfast Menu🥣 🥗 🐔 🥑🍞')
+streamlit.text('🥣 🥗 🐔 🥑🍞🥣 🥗 🐔 🥑🍞🥣 🥗 🐔 🥑🍞Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 🥗 🐔 🥑🍞🥣 🥗 🐔 🥑🍞Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥣 🥗 🐔 🥑🍞Hard-Boiled Free-Range Egg🥣 🥗 🐔 🥑🍞')
