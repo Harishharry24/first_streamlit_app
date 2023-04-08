f53d35d9a5da398779c26de227eca44e314358b8
Harishharry24 <130252325+Harishharry24@users.noreply.github.com>
2023-04-08 22:24:06 +0100

    Update streamli_app.py
    importedpandas library

--- streamli_app.py
+++ streamli_app.py
@@ -14,3 +14,7 @@ streamlit.text('🥣 🥗 🐔 🥑🍞🥣 🥗 🐔 🥑🍞Kale, Spinach & Ro
 streamlit.text('🥣 🥗 🐔 🥑🍞Hard-Boiled Free-Range Egg🥣 🥗 🐔 🥑🍞')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
