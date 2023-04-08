56480b752d66774f986cd21795d68a3f6196404f
Harishharry24 <130252325+Harishharry24@users.noreply.github.com>
2023-04-08 22:18:41 +0100

    Update streamli_app.py
    First I gave capital S typo error
    
    then I added header, text and now three breakfast menu

--- streamli_app.py
+++ streamli_app.py
@@ -7,3 +7,8 @@ streamlit.title( 'chapathi with ghee and peas')
 streamlit.text('hey how are you')
 
 streamlit.header('harish will get job in Amazon')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
