7cf8a2fcf03e5fec8675bb12735a9af0547767c1
Harishharry24 <130252325+Harishharry24@users.noreply.github.com>
2023-04-09 20:25:42 +0100

    Update streamli_app.py

--- streamli_app.py
+++ streamli_app.py
@@ -31,10 +31,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.write('The user entered ', fruit_choice)  #adda text entry box and send input to fruity vice as part of api call
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
 
 
 #streamlit.text(fruityvice_response.json()) #just writes data to the screen
